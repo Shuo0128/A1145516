cf4c2de939378bfd6ca8276b9c68949dee77e4cb
邱崇碩 <tom2007128@gmail.com>
2026-04-28 19:30:35 +0800

    Add character frequency counter for input sentences
    Implement character frequency counting from input sentences and output the frequency of each character in descending order.

---
+++ Problem/UVa_10008.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    int t, max = 0;
+    cin >> t;
+	getchar(); // 濾掉換行符號
+    int table[26] = {};
+    while (t--) {
+        string sen;
+        getline(cin, sen);
+        for (int i = 0; i < sen.length(); i++) {
+            if ('A' <= sen[i] && sen[i] <= 'Z') {
+                table[sen[i] - 'A']++;
+                if (table[sen[i] - 'A'] > max) {
+                    max = table[sen[i] - 'A'];
+                }
+            }
+            if ('a' <= sen[i] && sen[i] <= 'z') {
+                table[sen[i] - 'a']++;
+                if (table[sen[i] - 'a'] > max) {
+                    max = table[sen[i] - 'a'];
+                }
+            }
+        }
+    }
+    for (int j = max; j >= 1; j--) {
+        for (int k = 0; k <= 25; k++) {
+            if (table[k] == j) {
+                cout << char(k + 'A') << " " << j << "\n";
+            }
+        }
+    }
+}
