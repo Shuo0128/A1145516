24abc68eb3caf6582793ccff0dc05e180bb5c7dc
邱崇碩 <tom2007128@gmail.com>
2026-05-05 18:24:54 +0800

    Add B2-Sequence checker in UVa_11063.cpp
    Implemented logic to check if a sequence is a B2-Sequence based on input conditions.

--- Problem/UVa_11063.cpp
+++ Problem/UVa_11063.cpp
@@ -1,1 +1,41 @@
-
+#include<iostream>
+using namespace std;
+int main() {
+	int num[10000] = {};
+	int t, n, s;
+	int	casen = 1;
+	while (cin >> t) {
+		int booll = 1;
+		int increasing = 1;
+		for (int i = 0; i < t; i++) {
+			cin >> n;
+			if (n <= 0) {
+				booll = 0;
+			}
+			if (i > 0 && n <= num[i - 1]) {
+				increasing = 0;
+			}
+			num[i] = n;
+		}
+		int sum[20000] = {};
+		for (int j = 0; j < t; j++) {
+			int cur = num[j];
+			for (int k = j; k < t; k++) {
+				s = cur + num[k];
+				if (sum[s] == 0) {
+					sum[s] = 1;
+				}
+				else {
+					booll = 0;
+					break;
+				}
+			}
+		}
+		if (booll == 1 && increasing == 1) {
+			cout << "Case #" << casen++ << ": It is a B2-Sequence." << endl;
+		}
+		else {
+			cout << "Case #" << casen++ << ": It is not a B2-Sequence." << endl;
+		}
+	}
+}
