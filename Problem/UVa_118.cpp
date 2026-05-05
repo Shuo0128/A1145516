dd697d04dea1ace36131ebe4e95a08db07e11229
邱崇碩 <tom2007128@gmail.com>
2026-05-05 19:29:09 +0800

    Implement UVa 118 problem solution

--- Problem/UVa_118.cpp
+++ Problem/UVa_118.cpp
@@ -1,1 +1,81 @@
-
+#include<iostream>
+#include<string>
+#include<set>
+using namespace std;
+int main() {
+    int maxx = 0, maxy = 0;
+    cin >> maxx >> maxy;
+    set<string> scent;
+    int x = 0, y = 0;
+    char dir;
+    string cmd;
+    while (cin >> x >> y >> dir) {
+        cin >> cmd;
+        int lost = 0;
+        for (char c : cmd) {
+            if (c == 'L') {
+                if (dir == 'N') {
+                    dir = 'W';
+                }
+                else if (dir == 'W') {
+                    dir = 'S';
+                }
+                else if (dir == 'S') {
+                    dir = 'E';
+                }
+                else if (dir == 'E') {
+                    dir = 'N';
+                }
+            }
+            if (c == 'R') {
+                if (dir == 'N') { 
+                    dir = 'E'; 
+                }
+                else if (dir == 'E') {
+                    dir = 'S';
+                }
+                else if (dir == 'S') {
+                    dir = 'W'; 
+                }
+                else if (dir == 'W') {
+                    dir = 'N';
+                }
+            }
+            if (c == 'F') {
+                int nx = x, ny = y;
+                if (dir == 'N') {
+                    ny++;
+                }
+                else if (dir == 'S') {
+                    ny--;
+                }
+                else if (dir == 'E') {
+                    nx++;
+                }
+                else if (dir == 'W') {
+                    nx--;
+                }
+                if (nx < 0 || nx > maxx || ny < 0 || ny > maxy) {
+                    string key = to_string(x) + " " + to_string(y);
+                    if (scent.find(key) != scent.end()) {
+                        continue;
+                    }
+                    else {
+                        lost = 1;
+                        scent.insert(key);
+                        break;
+                    }
+                }
+                else {
+                    x = nx;
+                    y = ny;
+                }
+            }
+        }
+        cout << x << " " << y << " " << dir;
+        if (lost == 1) {
+            cout << " LOST";
+        }
+        cout << endl;
+    }
+}
